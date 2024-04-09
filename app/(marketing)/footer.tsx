6c5d20e3c7153f33e1ba0dade5e80ef5c34e44a9
StarStudio <charlottewiltshire27@outlook.com>
2024-04-09 18:45:41 +0300

    Added footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -7,13 +7,53 @@ export const Footer = () => {
             <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
                 <Button size="lg" variant="ghost" className="w-full">
                     <Image
-                        src="/hr.svg"
+                        src="/language/hr.svg"
                         alt="Croatian"
                         height={32}
                         width={40}
                         className="mr-4 rounded-md"
                     />
                     Croatian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/language/es.svg"
+                        alt="Spanish"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Spanish
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/language/fr.svg"
+                        alt="French"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    French
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/language/it.svg"
+                        alt="Italian"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Italian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/language/jp.svg"
+                        alt="Japanese"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Japanese
                 </Button>
             </div>
         </footer>
