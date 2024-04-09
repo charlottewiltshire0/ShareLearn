7e66187b53d83478b81ea9589cdecdba9c22dc3a
StarStudio <charlottewiltshire27@outlook.com>
2024-04-09 16:28:16 +0300

    Created css for buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
   return (
       <div className="text-2xl">
-        <p>Hello ShareLearn</p>
+        <Button>Click Me!</Button>
       </div>
   )
 }
